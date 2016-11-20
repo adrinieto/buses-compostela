f7626d48eb38b7eebfc2b86f2c29c9f7ba3bd6b6
Adrián Nieto <adrian.nieto7@gmail.com>
2016-11-20 18:25:11 +0100

    Show incidences with a modal

--- src/BusRoutes.jsx
+++ src/BusRoutes.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import loading from './loading.svg'
+import loading_icon from './loading.svg'
+import alert_icon from './alert.svg'
+import IncidencesModal from './IncidencesModal'
 
 const ENDPOINT_ROUTES = 'https://app.tussa.org/tussa/api/lineas';
 const ENDPOINT_ROUTE_ID = 'https://app.tussa.org/tussa/api/lineas/';
 
+
+function AlertIcon(props) {
+    return (
+        <img id="alert_icon"
+            onClick={() => props.onClick()}
+            className="clickable"
+            src={alert_icon} alt="alert" />
+    )
+}
+
 function LoadingIcon(props) {
     return (
-        <img id="loading" src={loading} alt="loading" />
+        <img id="loading_icon" src={loading_icon} alt="loading" />
     )
 }
 
@@ -16,7 +28,7 @@ function BusStop(props) {
     const zona = stop.zona ? `(${stop.zona}) ` : "";
     return (
         <li>
-            {stop.codigo} - {zona}{stop.nombre}
+            {stop.codigo}- {zona}{stop.nombre}
         </li>
     )
 }
@@ -45,69 +57,83 @@ class Route extends Component {
         this.state = {
             details: null,
             loading: false,
-            displayDirectionId: null
+            displayDirectionId: null,
+            showIncidences: null,
 
         }
+    }
+    loadData() {
+        this.setState({
+            loading: true
+        })
+        axios.get(ENDPOINT_ROUTE_ID + this.props.route.id)
+            .then(response => {
+                this.setState({
+                    loading: false,
+                    details: response.data,
+                })
+            }).catch(function (error) {
+                console.log(error);
+            });
     }
     handleClickOnHeader() {
         this.setState({
             displayDirection: null
         });
         if (!this.state.loading && !this.state.details) {
-            this.setState({
-                loading: true
-            })
-            axios.get(ENDPOINT_ROUTE_ID + this.props.route.id)
-                .then(response => {
-                    this.setState({
-                        loading: false,
-                        details: response.data,
-                    })
-                }).catch(function (error) {
-                    console.log(error);
-                });
+            this.loadData()
         }
         this.props.onClick();
     }
     handleClickOnDirection(directionId) {
         this.setState({
-            displayDirectionId:
-
-            directionId === this.state.displayDirectionId ? null : directionId
-
+            displayDirectionId: directionId === this.state.displayDirectionId ? null : directionId
+        })
+    }
+    handleClickOnAlert(routeId) {
+        if (!this.state.details){
+            this.loadData();
+        }
+        this.setState({
+            showIncidences: routeId
+        })
+    }
+    handleCloseModal() {
+        this.setState({
+            showIncidences: null,
         })
     }
     render() {
         const route = this.props.route;
         const incidencias = route.incidencias ?
-            ' (Con incidencias)' : null;
+            <AlertIcon onClick={() => this.handleClickOnAlert(route.id)} /> : null;
         var directions = null;
         if (this.props.display && this.state.details) {
             directions = this.state.details.trayectos.map(direction => {
                 return <RouteDirection
                     onClick={() => this.handleClickOnDirection(direction.sentido)}
                     display={this.state.displayDirectionId === direction.sentido}
-
                     key={route.id + direction.sentido}
                     routeId={route.id}
                     direction={direction} />
             });
         }
-
         return (
             <div>
                 <h2 className="selectable" style={{ color: route.estilo }}
                     onClick={() => this.handleClickOnHeader()}>
-                    {route.sinoptico} - {route.nombre}
-                    {incidencias}
+                    {route.sinoptico}- {route.nombre}
                 </h2>
+                {incidencias}
                 {this.state.loading &&
                     <LoadingIcon />
                 }
                 {this.props.display && this.state.details &&
                     <ul>{directions}</ul>
                 }
-
+                {this.state.showIncidences && this.state.details &&
+                    <IncidencesModal incidences={this.state.details.incidencias} onClose={() => this.handleCloseModal()} />
+                }
             </div>
         )
     }
